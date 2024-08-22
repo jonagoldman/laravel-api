a4c0e41c7e2e56c946d381f5d38ff60a7a69331f
Jona Goldman <jonagoldman@gmail.com>
2024-08-22 15:36:06 -0300

    Add Giphy variables

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'giphy' => [
+        'url' => env('GIPHY_URL'),
+        'key' => env('GIPHY_API_KEY'),
+    ],
+
 ];
