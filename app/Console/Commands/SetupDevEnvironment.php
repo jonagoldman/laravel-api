f90a9c1fb6a020cea63aab0c207338886f6ab962
Jona Goldman <jonagoldman@gmail.com>
2024-08-25 16:02:30 -0300

    Setup environment command

---
+++ app/Console/Commands/SetupDevEnvironment.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class SetupDevEnvironment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup:dev';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setup the development environment';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Setting up development environment');
+
+        // Drop all tables, run migrations and seed
+        $this->call('migrate:fresh', ['--seed' => true]);
+
+        // Create test user
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $this->info('All done. Have fun!');
+    }
+}
